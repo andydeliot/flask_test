22474154b4426a6c5db4743041871b3fb50aae8b
Andy <A.andy@hotmail.fr>
2021-01-28 18:51:43 +0100

    first commit

---
+++ project.py
@@ -0,0 +1,21 @@
+#Python3
+
+# Faire une simple page web en Flask.
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+
+
+
+if __name__ == "__main__":
+    pass
